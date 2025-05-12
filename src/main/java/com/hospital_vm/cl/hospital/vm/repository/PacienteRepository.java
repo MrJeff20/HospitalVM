65df76c507b877612af0503b2bfc3dc7099c2ad4
MrJeff20 <je.carbonell@duocuc.cl>
2025-05-12 14:10:38 -0400

    Parte 2 (realizar PacienteService en adelante)

---
+++ src/main/java/com/hospital_vm/cl/hospital/vm/repository/PacienteRepository.java
@@ -0,0 +1,23 @@
+package com.hospital_vm.cl.hospital.vm.repository;
+
+import com.hospital_vm.cl.hospital.vm.model.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PacienteRepository extends JpaRepository<Paciente, Long>{
+
+    //JPQL
+    @Query("SELECT p FROM Paciente p WHERE p.apellidos= :apellido")
+    List<Paciente> findByApellidos(String apellidos);
+
+    //SQL Nativo
+    @Query(value = "SELECT * FROM paciente WHERE correo = :correo", nativeQuery = true)
+    Paciente findByCorreo(@Param("correo")String correo);
+
+    List<Paciente> findByNombresAndApellidos(String nombres, String apellidos);
+}
